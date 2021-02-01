4f37f228a44452f548179270eb91ec70e99758aa
SaphireVert <jerome.cosandey@gmail.com>
2021-02-01 13:25:28 +0100

    [Add] Visual Studio solution

---
+++ tower-defense/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tower_defense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
