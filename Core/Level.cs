e9826431d3e6fe9df2707ed9aa7371855c400173
kilian-aiello <kilian.aiello@gmail.com>
2021-04-19 13:57:18 +0200

    Affichage level sur l'interface

--- Core/Level.cs
+++ Core/Level.cs
@@ -21,9 +21,11 @@ namespace tower_defense.Core
         //Constante de multiplication pour le calcul du prix du level
         private const int _levelPriceConst = 50;
 
+        
+
         public Level()
         {
-            this.levelNo = 2;
+            this.levelNo = 1;
             this.levelMax = 10;
             this.levelPrice = 0;
         }
--- MyGame.cs
+++ MyGame.cs
@@ -2,29 +2,21 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using tower_defense.Core;
-using System;
 
 namespace tower_defense
 {
     public class MyGame : Game
     {
-        
 
-        //1.3.1
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
-        //Constantes qui définissent la taille de la fenetre 1.3.2 
-        /*public const int WINDOW_WIDTH = 800;
-        public const int WINDOW_HEIGHT = 500;*/
-
-        //1.5.1
-        /*Monde monde;
-
-        Player player;*/
+        //Police d'écriture pour l'affichage du level
+        private SpriteFont _gameplayfont;
+        private string _levelNoString;
 
         MenuBase menu;
         MoneyGenerator moneyGenerator = new MoneyGenerator();
+        Level level = new Level();
 
         public MyGame()
         {
@@ -41,33 +33,19 @@ namespace tower_defense
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            //1.5.2
-            
-            /*monde = new Monde();
-            //Initialisation player (nombreDeSprites, )
-            player = new Player(1, 71, 71);*/
             base.Initialize();
 
             this.menu = new MenuMain();
             moneyGenerator.GenerateMoney(1000);
+            _levelNoString = level.levelNo.ToString();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _gameplayfont = Content.Load<SpriteFont>("Fonts/gameplayFont");
 
-            // TODO: use this.Content to load your game content here
-            //1.5.3
             Resources.LoadImages(this.Content);
-            /*monde.Texture = Content.LoadLocalized<Texture2D>("towerSprites/BaseTowerTest");
-            monde.Position = new Vector2(0,0);*/
-            /*monde.Source = new Rectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);*/
-
-            //Chargement et positionnement du perso
-            
-            //Positionnement
-            /*player.Position = new Vector2(201, 71);*/
         }
 
         protected override void Update(GameTime gameTime)
@@ -75,14 +53,7 @@ namespace tower_defense
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-            //1.3.4
             this.menu.Update();
-
-            /*_graphics.PreferredBackBufferWidth = WINDOW_WIDTH;  
-            _graphics.PreferredBackBufferHeight = WINDOW_HEIGHT;*/
-
-            /*player.UpdateFrame(gameTime);*/
 
             base.Update(gameTime);
         }
@@ -91,12 +62,12 @@ namespace tower_defense
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
-           
             _spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, null, null, null, null);
-                this.menu.Draw(this._spriteBatch);
-           /* monde.Draw(_spriteBatch);
-           player.DrawAnimation(_spriteBatch);*/
+            this.menu.Draw(this._spriteBatch);
+
+
+            _spriteBatch.DrawString(_gameplayfont, "Level: " + _levelNoString, new Vector2(600, 450), Color.Black);
+
             _spriteBatch.End();
             base.Draw(gameTime);
         }
