0408bb3e51502632e8a7589aa2433582f2613841
saphirevert <jerome.cosandey@gmail.com>
2021-04-26 21:43:42 +0200

    basic  feature

---
+++ tg-bot/app/index.js
@@ -0,0 +1,7 @@
+const TeleBot = require('telebot');
+const secrets = require('./secrets.json');
+const bot = new TeleBot(secrets.BOT_TOKEN);
+
+bot.on('/bonjour', (msg) => msg.reply.text(`Bonjour ${msg.from.username}`));
+
+bot.start();
