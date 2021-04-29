0383a1ef28c33bf11629030034dfd20f2bce1c1b
kilian-aiello <kilian.aiello@gmail.com>
2021-04-29 19:17:01 +0200

    Création du projet VS

--- tg-bot/app/index.js
+++ tg-bot/app/index.js
@@ -4,4 +4,6 @@ const bot = new TeleBot(secrets.BOT_TOKEN);
 
 bot.on('/bonjour', (msg) => msg.reply.text(`Bonjour ${msg.from.username}`));
 
+bot.on('/bonsoir', (msg) => msg.reply.text(`Bonsoir ${msg.from.username}`));
+
 bot.start();
