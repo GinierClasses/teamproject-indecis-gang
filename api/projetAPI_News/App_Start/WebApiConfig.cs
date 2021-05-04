070b0752f2b5bf38e9bc735684d7252f04636484
SaphireVert <jerome.cosandey@gmail.com>
2021-05-04 18:16:06 +0200

    Nouveau controlleur pour une route "/test"

--- api/projetAPI_News/App_Start/WebApiConfig.cs
+++ api/projetAPI_News/App_Start/WebApiConfig.cs
@@ -19,6 +19,16 @@ namespace projetAPI_News
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi1",
+                routeTemplate: "api/Test/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            Console.WriteLine("jesaiscpaspropre");
+            //Console.OpenStandardOutput();
+            System.Diagnostics.Debug.WriteLine("test");
+            App_Start.Index.TestFunc();
         }
     }
 }
