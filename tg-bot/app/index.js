8c251f44cd17a125272da863db3209b7fc39fa1c
RuCaVi <50481251+RuCaVi@users.noreply.github.com>
2021-05-09 18:49:22 +0200

    Ajout de la commande pour activer/desactiver

--- tg-bot/app/index.js
+++ tg-bot/app/index.js
@@ -1,9 +1,22 @@
 const TeleBot = require('telebot');
 const secrets = require('./secrets.json');
 const bot = new TeleBot(secrets.BOT_TOKEN);
+var notification = true;
 
-bot.on('/bonjour', (msg) => msg.reply.text(`Bonjour ${msg.from.username}`));
+bot.on('/bonjour', function(msg) { 
+    console.log(msg);
+    return msg.reply.text(`Bonjour ${msg.from.username}`);
+});
 
-bot.on('/bonsoir', (msg) => msg.reply.text(`Bonsoir ${msg.from.username}`));
+bot.on('/bonsoir', function(msg) { 
+    console.log(msg);
+    return msg.reply.text(`Bonsoir ${msg.from.username}`);
+});
+
+bot.on('/notification', function(msg) {
+    notification = notification == true ? false : true;
+    console.log(notification);
+    return notification ? msg.reply.text("Les notifications ont été activées !") : msg.reply.text("Les notifications ont été désactivées !");
+});
 
 bot.start();
