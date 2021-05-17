de6aa0233d144c4be6fb562b71a5e5401f0d4dd5
kilian-aiello <kilian.aiello@gmail.com>
2021-05-17 14:14:58 +0200

    Création de la classe author, category et news

---
+++ api/projetAPI_News/Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projetAPI_News.Models
+{
+    public class Author
+    {
+        int IdAuthor;
+        string AuthorName;
+
+        public Author(int idAuthor, string authorName)
+        {
+            IdAuthor = idAuthor;
+            AuthorName = authorName;
+        }
+    }
+}
---
+++ api/projetAPI_News/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projetAPI_News.Models
+{
+    public class Category
+    {
+        int IdCategory;
+        string CategoryName;
+
+        public Category(int idCategory, string categoryName)
+        {
+            IdCategory = idCategory;
+            CategoryName = categoryName;
+        }
+    }
+}
