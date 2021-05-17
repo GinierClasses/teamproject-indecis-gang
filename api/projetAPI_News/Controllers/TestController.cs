c33a964c3b9f6e74a6b8527a34c46b430205e259
SaphireVert <jerome.cosandey@gmail.com>
2021-05-17 08:59:56 +0200

    DB connection made

--- api/projetAPI_News/Controllers/TestController.cs
+++ api/projetAPI_News/Controllers/TestController.cs
@@ -5,6 +5,10 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
 namespace projetAPI_News.Controllers
 {
     public class TestController : ApiController
@@ -12,6 +16,22 @@ namespace projetAPI_News.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
+            
+            string constr = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                using (MySqlCommand cmd = new MySqlCommand("SELECT * FROM t_user"))
+                {
+                    using (MySqlDataAdapter sda = new MySqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        System.Diagnostics.Debug.WriteLine(cmd);
+                    }
+                }
+                
+            }
+            
             return new string[] { "value1", "value2" };
         }
 
